b51cd814dc8cfeea43a7871841a130eddd48d423
NicolasEHT <bombillox36@gmail.com>
2020-03-24 22:16:20 -0400

    Segundo Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<ul>
+		<li><a href="formulario.html">Formulario</li>
+		<li><a href="#">Modulo 1</li>
+		<li><a href="#">Modulo 2</li>
+	</ul>
+</body>
+</html>
